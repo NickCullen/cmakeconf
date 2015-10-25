0f8f0b780d685fc89c29e90401d28addbb482850
Nick Cullen <nmcullen91@gmail.com>
2015-10-25 14:24:27 +0000

    Got visual studio filters working

--- cmake_templates.py
+++ cmake_templates.py
@@ -19,16 +19,24 @@ TDefinition = Template("add_definitions(-D${definition})")
 TIncludeDirectory = Template('include_directories("${dir}")')
 
 #for globbing source files in a dir
-TSourceGlob = Template('FILE(GLOB ${source_id} "${dir}/*.cpp")')
+TSourceGlob = Template('FILE(GLOB ${source_id} "${dir}/*.c*")')
+
+#for globbing header files in a dir
+THeaderGlob = Template('FILE(GLOB ${header_id} "${dir}/*.h*")')
+
+#template for source group (so they appear in VS filters etc.
+TSourceGroup = Template('source_group("${folder}" FILES $${${files}})\n')
 
 #for outputting an executable
-TExecutable = Template("add_executable(${project} $${SOURCES})\n")
+TExecutable = Template("add_executable(${project} $${SOURCES} $${HEADERS})\n")
 
 #template for appending to a cmake variable
 TAppendVariable = Template("set( ${var} $${${var}} $${${appendedval}})\n")
 
 #template for setting cmake variable
 TMakeVariable = Template('set (${var} ${value})\n')
+
+
 
 #-----Write Functions-----
 #Puts innerbody into TIfGuard template with the given condition
@@ -39,15 +47,25 @@ def WrapInGuard(condition, innerbody):
 def WriteProjectSettings(f, section):
 	#defaults
 	if "UseFolders" not in section.data: section.data["UseFolders"] = "OFF"
+	
+	#output
 	output = TProjectSettings.substitute(section.data)
 	f.write(output)
 	
+#writes required CMAKE variables to the file
 def WriteRequiredVariables(f):
-	variables = [dict(var="INCLUDES", value='""'), dict(var="SOURCES", value='""'), dict(var="LIBS", value='""') ]
+	#all required variables go here to initialise
+	variables = [
+		dict(var="INCLUDES", value='""'), 
+		dict(var="SOURCES", value='""'), 
+		dict(var="LIBS", value='""') 
+		]
+	
+	#write them to file	
 	for v in variables:
 		f.write(TMakeVariable.substitute(v))
 	
-	
+#definitions such as #defines 	
 def WriteDefinitions(f, sections):
 	#first write the one which is not platform specific
 	for s in sections:
@@ -62,6 +80,7 @@ def WriteDefinitions(f, sections):
 		#to the file if there is no condition. Else it writes the output wrapped in an ifguard to file
 		f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
 
+#project include directories
 def WriteIncludeDirectories(f, rootDir, sections):
 	#first write the one which is not platform specific
 	for s in sections:
@@ -71,36 +90,53 @@ def WriteIncludeDirectories(f, rootDir, sections):
 		output = ""
 		for d in dirs:
 			d = d if d.startswith('/') else "/"+d
-			output += TIncludeDirectory.substitute(dict(dir=rootDir+d)) + "\n"
+			headerID = d.replace('/','_')
 			
-		#This line look pretty complicated but all it really is doing is writing output
-		#to the file if there is no condition. Else it writes the output wrapped in an ifguard to file
-		f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
+			#add include directory
+			output = TIncludeDirectory.substitute(dict(dir=rootDir+d)) + "\n"
+			f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
+			
+			#glob all header files
+			output = THeaderGlob.substitute(dict(dir=rootDir+d, header_id=headerID)) + "\n"
+			f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
+			
+			#append to HEADERS variable
+			output = TAppendVariable.substitute(dict(var="HEADERS", appendedval=headerID))
+			f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
+			
+			#make source group so they appear in filters
+			d = d.replace('/','\\\\')
+			output = TSourceGroup.substitute(dict(folder="Header Files" + d, files=headerID))
+			f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
 		
+#project source directories
 def WriteSourceDirectories(f, rootDir, sections):
 	#first write the one which is not platform specific
 	for s in sections:
 		dirs = s.data[":"]
 
-		#gather definitions to be output
 		output = ""
 		for d in dirs:
+			d = d if d.startswith('/') else "/"+d
 			sourceID = d.replace('/','_')
 			
-			d = d if d.startswith('/') else "/"+d
+			#glob all source files
 			output = TSourceGlob.substitute(dict(dir=rootDir+d, source_id=sourceID)) + "\n"
-			
-			#This line look pretty complicated but all it really is doing is writing output
-			#to the file if there is no condition. Else it writes the output wrapped in an ifguard to file
 			f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
 			
-			#append to SOURCES cmake variable
-			f.write(TAppendVariable.substitute(dict(var="SOURCES", appendedval=sourceID)))
+			#append globbed source files to SOURCES cmake variable
+			output = TAppendVariable.substitute(dict(var="SOURCES", appendedval=sourceID))
+			f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
+			
+			#make source group so they appear in filters
+			d = d.replace('/','\\\\')
+			output = TSourceGroup.substitute(dict(folder="Source Files" + d, files=sourceID))
+			f.write(output if not s.HasCondition() else WrapInGuard(s.condition, output))
 		
 #Writes the module output section of the CmakeLists file
 def WriteModuleOutput(f, m):
 	name = m.settings.data["Name"]	#name of lib/exe
-	o = m.settings.data["Output"]				#build type (lib/exe)
+	o = m.settings.data["Output"]	#build type (lib/exe)
 	if "exe" in o:
 		f.write(TExecutable.substitute(dict(project=name)))
 	
